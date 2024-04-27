c363c56be0e01b61c07a4dbd7335fb4f88a6a35f
Parsons <gongyingparsons@gmail.com>
2024-04-28 01:35:58 +0800

    nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,14 @@ export default defineNuxtConfig({
             brokerUrl: 'ws://54.238.232.62:8080/legendsOfTheThreeKingdoms',
         },
     },
+    production: {
+        runtimeConfig: {
+            public: {
+                apiBaseUrl: 'https://scolley31.com/',
+                brokerUrl: 'wss://scolley31.com/legendsOfTheThreeKingdoms',
+            },
+        },
+    },
     devtools: { enabled: true },
     modules: ['@nuxtjs/tailwindcss'],
 })
